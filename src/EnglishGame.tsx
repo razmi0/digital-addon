dc8659fe4a7a2c4983bb627f2ea704ef9a9f4984
razmiO <thomas.cuesta31@gmail.com>
2024-12-04 14:37:11 +0100

    update

--- src/EnglishGame.tsx
+++ src/EnglishGame.tsx
@@ -1,46 +1,19 @@
 import { Fragment } from "react/jsx-runtime";
+import data from "./data.json" assert { type: "json" };
+
+const devEnv = import.meta.env.DEV;
 
 const EnglishGame = () => {
   return (
     <>
-      <MedievalInternetGrid />
+      {devEnv}
+      <DevInfo />
     </>
   );
 };
 
-const MedievalInternetGrid = () => {
-  const data = [
-    {
-      name: "route",
-      term: "bande-passante",
-      category: "infra",
-      description: "permet de faire circuler des données entre serveur",
-    },
-    { name: "entrepot", term: "base de donnée", category: "infra", description: "stocke des informations" },
-    { name: "porte muraille", term: "pare-feu", category: "infra", description: "protège de 1/2/3 hacker" },
-    { name: "hopital", term: "sys de maintenance", category: "infra", description: "previent de 1/2/3 bug" },
-    { name: "brigant", term: "bug", category: "action", description: "bloque un batiment un tour" },
-    { name: "marché", term: "API", category: "infra", description: "permet echange avec d'autres joueurs" },
-    {
-      name: "architecte office",
-      term: "baie de serveur",
-      category: "infra",
-      description: "genere des serveur/ par tour",
-    },
-    {
-      name: "bibliothèque",
-      term: "moteur de recherche",
-      category: "infra",
-      description: "permet de piocher plus d'action",
-    },
-    { name: "postoffice", term: "serveur smtp", category: "infra", description: "upgrade le marché" },
-    { name: "voleur", term: "hacker", category: "action", description: "pique des octets" },
-    { name: "garde", term: "HTTPS", category: "action", description: "upgrade une route en HTTPS" },
-    { name: "embuscade", term: "downgrade attack", category: "action", description: "downgrade une route en HTTP" },
-    { name: "blé", term: "octet", category: "ressources", description: "ressource principal" },
-    { name: "maison", term: "serveur", category: "ressources", description: "ressource principal" },
-  ];
-
+const DevInfo = () => {
+  const { dev_cards } = data;
   return (
     <div
       style={{
@@ -57,7 +30,7 @@ const MedievalInternetGrid = () => {
       <div style={{ fontWeight: "bold", borderBottom: "1px solid black", padding: "0.5rem" }}>Description</div>
 
       {/* Grid Rows */}
-      {data.map((item, index) => (
+      {dev_cards.map((item, index) => (
         <Fragment key={index}>
           <div style={{ borderBottom: "1px solid black", padding: "0.5rem" }}>{item.name}</div>
           <div style={{ borderBottom: "1px solid black", padding: "0.5rem" }}>{item.term}</div>
